09708214ca08625a1d09447f748a605a157df3e6
Luca <luca.lorenzetti0@gmail.com>
2021-04-08 17:21:02 +0200

    Inserita select

--- js/script.js
+++ js/script.js
@@ -4,14 +4,18 @@
 *    https://flynn.boolean.careers/exercises/api/array/music
 *    avremo a disposizione una decina di dischi musicali.
 *    Utilizzando vue, stampiamo a schermo una card per ogni album.
-*    BONUS: Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+*    BONUS: Creare una select con tutti i generi dei dischi. 
+            In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
 *    BONUS 2: Ordinare i dischi per anno di uscita. 
 */
 
 let app = new Vue({
     el: '#root',
     data:{
-        discs: []
+        discs: [],
+        discsFiltered: [],
+        genres: ["All"],
+        filters: []
     },
     methods: {
         getListDisc(){
@@ -19,9 +23,24 @@ let app = new Vue({
             axios.get("https://flynn.boolean.careers/exercises/api/array/music")
             .then( (response)=>{
                 this.discs = response.data.response;
+                this.discsFiltered = this.discs;
+                 this.getGenres();
             });        
-           
+        },
+        getGenres(){
+            this.discs.forEach(element => {
+            
+                if( !this.genres.includes(element.genre) ){
+                    this.genres.push(element.genre);
+                }
+            });
+        },
+        getFilters(filters){
+            this.discsFiltered = this.discs.filter(element => {
+                return filters.includes(element.genre) || filters.includes("All"); 
+            });
         }
+        
     },
     created(){
         this.getListDisc();
